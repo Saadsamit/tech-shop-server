563c839c218c8c478168844ef0574a6413fbc664
MD SAMIT INTESAR <saadsamit64@gmail.com>
2023-10-18 21:40:00 +0600

    server brands api completed

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.amhrtlq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -16,26 +16,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+    const database = client.db("Tech-Shop");
+    const brand = database.collection("brand");
+    app.get("/brands", async (req, res) => {
+      const getBrands = brand.find();
+      const result = await getBrands.toArray();
+      res.send(result);
+    });
+    app.post("/brands", async (req, res) => {
+      const obj = req.body;
+      const result = await brand.insertOne(obj);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send('server is runing')
-})
+app.get("/", (req, res) => {
+  res.send("server is runing");
+});
 
-
-app.listen(port)
+app.listen(port);
